7ef28941a0056e1daaae3b99b02f427bdee9938f
Saurabh Singh <saurabh28720@gmail.com>
2025-06-25 23:57:57 +0530

    Basic project setup for nest with mongo crud

---
+++ command.ts
@@ -0,0 +1,11 @@
+// To install the nestjs cli globly if not run use sudo: npm i -g @nestjs/cli
+// To Generate new nest project: nest new .     (Here dot is give the current folder name)
+
+// To Generate new nest project: nest new project_or_projectfolderName
+// To install mongoose dependency: npm i @nestjs/mongoose mongoose
+// To Start The Application by command: npm run start:dev
+
+// Now to make the Model, controller & service.
+// To make model: nest g model book
+// To make controller: nest g controller book
+// To make service: nest g service book --no-spec  (Here --no-spec is use for not to generate the test file)
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookModule } from './book/book.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1/nestDb'), BookModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
